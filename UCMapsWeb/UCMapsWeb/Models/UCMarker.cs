aa9266edbed42fc51627b6f7e64334587f94e60d
JaredWalden00 <jaredwalden00@gmail.com>
2024-12-26 17:10:38 -0500

    Update

---
+++ UCMapsWeb/UCMapsWeb/Models/UCMarker.cs
@@ -0,0 +1,16 @@
+﻿using GoogleMapsComponents.Maps;
+using OneOf;
+using System.Text.Json.Serialization;
+using UCMaps.Components.Models;
+
+public class UCMarker
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Lat { get; set; }
+    public double Lng { get; set; }
+    public UserSession User { get; set; }
+    public int StillThereCount { get; set; } = 0;
+    public int NotThereCount { get; set; } = 0;
+}
---
+++ UCMapsWeb/UCMapsWeb/Models/UserSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UCMaps.Components.Models
+{
+    public class UserSession
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
